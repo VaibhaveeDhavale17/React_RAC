805bcf3800d886c9785fb63e70a8d9e7b34f548a
Vaibhavee Dhavale <vaibhaveedhawale@gmail.com>
2024-02-02 11:37:52 +0530

    Update page

--- frontend/src/dashboard/EditItems.jsx
+++ frontend/src/dashboard/EditItems.jsx
@@ -63,37 +63,37 @@ const EditItems = () => {
           <div className="col-lg-8 mx-auto mt-5 p-5 "  style={{ backgroundColor: 'white', boxShadow: '0 4px 8px rgba(0, 0, 1, 0.1)' }} >
 
           <div className="flex flex-col items-center mb-2 ">
-  <h5 className="font-bold mb-4 text-center">Update The Item</h5>
+  <h5 className="font-poppins text-[#000000] font-bold mb-4 text-center">Update The Item</h5>
 </div>
     <form onSubmit={handleUpdate}>
             <div className="row g-3">
               <div className="col-md-6">
-                <label className="form-label font-poppins text-[#000000] font-bold" htmlFor="refNumber" >Product Reference Number</label>
+                <label className="label-style font-poppins font-bold" htmlFor="refNumber" >Product Reference Number</label>
                 <input type="text" id="refNumber" name="refNumber" className="form-control" placeholder="" value={values.refNumber} />
               </div>
               <div className="col-md-6">
-                <label className="form-label font-poppins text-[#000000] font-bold" htmlFor="productName" >Product Name</label>
+                <label className="label-style font-poppins font-bold" htmlFor="productName" >Product Name</label>
                 <div className="input-group input-group-merge">
                   <input className="form-control" type="text" id="productName" name="productName" value={values.productName} onChange={e=> setValues({...values,productName:e.target.value})} />
                 </div>
               </div>
 
               <div className="col-12">
-                <label className="form-label font-poppins text-[#000000] font-bold" htmlFor="productDescription" >Product Decription</label>
+                <label className="label-style font-poppins font-bold" htmlFor="productDescription" >Product Decription</label>
                 <textarea name="productDescription" className="form-control" id="productDescription" rows="2" placeholder="" value={values.productDescription} onChange={e=> setValues({...values,productDescription:e.target.value})}></textarea>
               </div>
               <div className="col-md-6">
-                <label className="form-label font-poppins text-[#000000] font-bold" htmlFor="productPrice">Product Price</label>
+                <label className="label-style font-poppins font-bold" htmlFor="productPrice">Product Price</label>
                 <input type="text" id="productPrice" name="productPrice" className="form-control phone-mask" value={values.productPrice} onChange={e=> setValues({...values,productPrice:e.target.value})} />
               </div>
 
               <div className="col-md-6">
-                <label className="form-label font-poppins text-[#000000] font-bold" htmlFor="productCategory" >Product Category</label>
+                <label className="label-style font-poppins font-bold" htmlFor="productCategory" >Product Category</label>
                 <input type="text" id="productCategory" name="productCategory" className="form-control phone-mask" value={values.productCategory} onChange={e=> setValues({...values,productCategory:e.target.value})}/>
               </div>
 
               {/* <div className="col-md">
-                <label className="form-label font-poppins text-[#000000] font-bold" htmlFor="month" id='month' name="month">Entry Month</label>
+                <label className="label-style font-poppins font-bold" htmlFor="month" id='month' name="month">Entry Month</label>
                 <div className="position-relative"><select id="month" className="select2 form-select select2-hidden-accessible" data-allow-clear="true" data-select2-id="month" tabIndex="-1" aria-hidden="true">
                   <option value={values.month} data-select2-id="2"></option>
                   <option value="01">January</option>
@@ -113,7 +113,7 @@ const EditItems = () => {
               </div> */}
 
 <div className="col-md">
-  <label className="form-label font-poppins text-[#000000] font-bold" htmlFor="month">
+  <label className="label-style font-poppins font-bold" htmlFor="month">
     Entry Month
   </label>
   <div className="position-relative">
@@ -175,43 +175,43 @@ const EditItems = () => {
 
 
               <div className="col-md-6">
-                <label className="form-label font-poppins text-[#000000] font-bold" htmlFor="productEntryDate">Product Entry Date</label>
+                <label className="label-style font-poppins font-bold" htmlFor="productEntryDate">Product Entry Date</label>
                 <input type="text" id="productEntryDate" name="productEntryDate" className="form-control phone-mask" placeholder=""     value={values.productEntryDate ? new Date(values.productEntryDate).toLocaleDateString() : ''}
  onChange={e=> setValues({...values,productEntryDate:e.target.value})} />
               </div>
 
               <div className="col-md-6">
-                <label className="form-label font-poppins text-[#000000] font-bold" htmlFor="productExpiryDate">Product Expiry Date</label>
+                <label className="label-style font-poppins font-bold" htmlFor="productExpiryDate">Product Expiry Date</label>
                 <input type="text" id="productExpiryDate" name="productExpiryDate" className="form-control phone-mask" placeholder=""     value={values.productExpiryDate ? new Date(values.productExpiryDate).toLocaleDateString() : ''}
  onChange={e=> setValues({...values,productExpiryDate:e.target.value})} />
               </div>
 
 
               <div className="col-md-6">
-                <label className="form-label font-poppins text-[#000000] font-bold" htmlFor="numOfProducts">Number of Products</label>
+                <label className="label-style font-poppins font-bold" htmlFor="numOfProducts">Number of Products</label>
                 <input type="text" id="numOfProducts" name="numOfProducts" className="form-control phone-mask" placeholder="" value={values.numOfProducts} onChange={e=> setValues({...values,numOfProducts:e.target.value})} />
               </div>
               
               <div className="col-md-6">
-                <label className="form-label font-poppins text-[#000000] font-bold" htmlFor="tax">tax</label>
+                <label className="label-style font-poppins font-bold" htmlFor="tax">tax</label>
                 <input type="text" id="tax" name="tax" className="form-control" placeholder="" value={values.taxPercentage} onChange={e=> setValues({...values,taxPercentage:e.target.value})} />
               </div>
               <div className="col-md-6">
-                <label className="form-label font-poppins text-[#000000] font-bold" htmlFor="cgst">CGST</label>
+                <label className="label-style font-poppins font-bold" htmlFor="cgst">CGST</label>
                 <input type="text" id="cgst" name="cgst" className="form-control" placeholder="" value={values.cgstPercentage} onChange={e=> setValues({...values,cgstPercentage:e.target.value})} />
               </div>
               <div className="col-md">
-                <label className="form-label font-poppins text-[#000000] font-bold" htmlFor= "sgst">SGST</label>
+                <label className="label-style font-poppins font-bold" htmlFor= "sgst">SGST</label>
                 <input type="text" id="sgst" name="sgst" className="form-control" placeholder="" value={values.sgstPercentage} onChange={e=> setValues({...values,sgstPercentage:e.target.value})} />
               </div>
 
               {/* <div className="col-md">
-                <label className="form-label font-poppins text-[#000000] font-bold" htmlFor= "totalPrice">Total Price</label>
+                <label className="label-style font-poppins font-bold" htmlFor= "totalPrice">Total Price</label>
                 <input type="text" id="totalPrice" name="totalPrice" className="form-control" placeholder="" value={values.totalPrice} onChange={e=> setValues({...values,totalPrice:e.target.value})} />
               </div>
   */}
               <div className="flex justify-center">
-        <button className="flex flex-col bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+        <button className=" font-poppins text-[#000000] flex flex-col bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
           Update
         </button>
 </div>
